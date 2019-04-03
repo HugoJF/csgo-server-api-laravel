a80f1b7938eaa8133a4fcb2babc55faba608d777
Hugo Jeller <hugo_jeller@hotmail.com>
2019-04-02 23:49:58 -0400

    documentation and broadcast mode

--- src/CsgoApi.php
+++ src/CsgoApi.php
@@ -24,6 +24,11 @@ class CsgoApi
 		$this->key = config('csgo-api.key');
 	}
 
+	/**
+	 * Sets broadcast mode on
+	 *
+	 * @return $this
+	 */
 	public function broadcast()
 	{
 		$this->broadcast = true;
@@ -31,6 +36,13 @@ class CsgoApi
 		return $this;
 	}
 
+	/**
+	 * Sets list of servers to execute commands
+	 *
+	 * @param string|array $server - server or list of servers to execute commands
+	 *
+	 * @return $this
+	 */
 	public function to($server)
 	{
 		if (!is_array($server)) {
@@ -42,39 +54,64 @@ class CsgoApi
 		return $this;
 	}
 
+	/**
+	 * Executes a list of commands in the set list of servers
+	 *
+	 * @param string|array $commands - command list to be executed
+	 * @param int          $delay    - delay in milliseconds when a single command is specified
+	 */
 	public function execute($commands, $delay = 0)
 	{
 		if (!is_array($commands)) {
 			$commands = [$commands, $delay];
 		}
 
-		foreach ($this->servers as $server) {
-			if ($info = $this->splitServerData($server)) {
-				['ip' => $ip, 'port' => $port] = $info;
+		$this->executeCommandList($commands, $this->servers);
+	}
 
-				foreach ($commands as $com) {
-					[$c, $delay] = $com;
+	/**
+	 * Execute list of commands on a list of servers
+	 *
+	 * @param array $commands - list of commands to be executed
+	 * @param array $servers  - list of servers to be executed on
+	 */
+	protected function executeCommandList($commands, $servers)
+	{
+		foreach ($commands as $com) {
+			[$command, $delay] = $com;
 
-					$this->sendCommandToServer($ip, $port, $c, $delay);
-				}
+			if ($this->broadcast) {
+				$this->sendToAll($command, $delay);
+			} else {
+				$this->executeOnServers($command, $delay, $servers);
 			}
 		}
 	}
 
-	protected function splitServerData($server)
+	/**
+	 * Execute a command in a list of servers
+	 *
+	 * @param string  $command - command to be executed
+	 * @param integer $delay   - delay in milliseconds
+	 * @param array   $servers - server list
+	 */
+	protected function executeOnServers($command, $delay, $servers)
 	{
-		$data = preg_split('/\:/', $server);
+		foreach ($servers as $server) {
+			if ($info = $this->splitServerData($server)) {
+				['ip' => $ip, 'port' => $port] = $info;
 
-		if (count($data) == 2) {
-			return [
-				'ip'   => $data[0],
-				'port' => $data[1],
-			];
-		} else {
-			return false;
+				$this->sendCommandToServer($ip, $port, $command, $delay);
+			}
 		}
 	}
 
+	/**
+	 * Send a command to all servers registered in the API
+	 *
+	 * @param string  $command - command to be executed
+	 * @param integer $delay   - delay in milliseconds
+	 */
 	protected function sendToAll($command, $delay)
 	{
 		$token = $this->key;
@@ -87,6 +124,14 @@ class CsgoApi
 		$curl->get();
 	}
 
+	/**
+	 * Sends a command to specified CS:GO server via API
+	 *
+	 * @param string  $ip      - CS:GO server IP
+	 * @param integer $port    - CS:GO server port
+	 * @param string  $command - Command to execute
+	 * @param integer $delay   - Delay to execute command in milliseconds
+	 */
 	protected function sendCommandToServer($ip, $port, $command, $delay)
 	{
 		$token = $this->key;
@@ -97,5 +142,26 @@ class CsgoApi
 		$curl->withData(compact('ip', 'port', 'command', 'delay', 'token'));
 
 		$curl->get();
+	}
+
+	/**
+	 * Split server address string to IP and port
+	 *
+	 * @param string $server - Server IP with Port
+	 *
+	 * @return array|bool - false on fail, associative array on success
+	 */
+	protected function splitServerData($server)
+	{
+		$data = preg_split('/\:/', $server);
+
+		if (count($data) == 2) {
+			return [
+				'ip'   => $data[0],
+				'port' => $data[1],
+			];
+		} else {
+			return false;
+		}
 	}
 }
